f872c42845510be42cbf26b3971988f18f635ecb
Kasey Arnold <kaseyarnold15@gmail.com>
2019-12-23 01:42:04 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,4 +2,4 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 puts(What a nutritious meal!)
-puts(A Plate of #{protein} with #{veg1} and #{veg2}.)
+puts(A Plate of #{Chicken} with #{Carrots} and #{Corn}.)
